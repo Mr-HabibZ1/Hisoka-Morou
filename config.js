efb9b97958f8b5be13de9dd28426fb36b977c95c
Mr-HabibZ1 <116667233+Mr-HabibZ1@users.noreply.github.com>
2022-12-24 02:21:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -14,14 +14,14 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://zenzapis.xyz': 'Your Key',
+	'https://danzzapi.xyz': 'Your Key',
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
+global.owner = ['6282160795453','6282182533461','6287893575487','6282160795453']
+global.premium = ['6282160795453']
+global.packname = 'Imam Habib Modz'
+global.author = 'Imam Habib'
 global.sessionName = 'hisoka'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
